33cf9d68498748accb3e8102a2fa001589322da3
tamir <tamir3837@gmail.com>
2024-08-19 13:10:14 +0300

    feat(routers server stracture)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
-from config import settings
+from app.api.main import api_router
+from app.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
+
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
